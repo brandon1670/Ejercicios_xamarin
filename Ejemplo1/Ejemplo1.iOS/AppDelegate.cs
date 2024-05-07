b59bb7b3729c1a91e40e0f50d3533b749edd53c3
Brandon1670 <Brandon.silva091670@gmail.com>
2024-05-06 23:49:19 -0700

    Se agrego la carpeta modelo vista , views, con clases para la creacion de una barra de navegacion, tambien se implemento el Bootstraper tanto en Andriod como en IOS, Entre otras cosas

--- Ejemplo1/Ejemplo1.iOS/AppDelegate.cs
+++ Ejemplo1/Ejemplo1.iOS/AppDelegate.cs
@@ -23,7 +23,6 @@ namespace Ejemplo1.iOS
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
             global::Xamarin.Forms.Forms.Init();
-            LoadApplication(new App());
 
             return base.FinishedLaunching(app, options);
         }
