dacc44077aedc6f4a2f9c53829ee47703128d457
Brandon1670 <Brandon.silva091670@gmail.com>
2024-05-08 00:29:57 -0700

    Se avanzo de la pagina 75 a la 81

--- Ejemplo1/Ejemplo1/ViewModels/ItemViewModel.cs
+++ Ejemplo1/Ejemplo1/ViewModels/ItemViewModel.cs
@@ -8,9 +8,22 @@ namespace Ejemplo1.ViewModels
     public class ItemViewModel : ViewModel
     {
         private readonly TodoItemRepository repository;
+
+        public TodoItem Item { get; set; }
+
+
         public ItemViewModel(TodoItemRepository repository)
         {
             this.repository = repository;
+            Item = new TodoItem() { Due = DateTime.Now.AddDays(1) };
         }
+
+        public ICommand Save => new Command(async () =>
+        {
+            await repository.AddOrUpdate(Item);
+            await Navigation.PopAsync();
+        });
+
+
     }
 }
--- Ejemplo1/Ejemplo1/ViewModels/MainViewModel.cs
+++ Ejemplo1/Ejemplo1/ViewModels/MainViewModel.cs
@@ -1,16 +1,52 @@
 ﻿using Ejemplo1.Repositories;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using Ejemplo1.Views;
+using System.Collections.ObjectModel;
+using System.Linq;
+using Xamarin.Forms;
+using Ejemplo1;
+using Ejemplo1.Models;
+using System;
 
 public class MainViewModel : ViewModel
 {
     private readonly TodoItemRepository repository;
     public MainViewModel(TodoItemRepository repository)
     {
+        repository.OnItemAdded += (sender, item) =>
+        Items.Add(CreateTodoItemViewModel(item));
+        repository.OnItemUpdated += (sender, item) =>
+        Task.Run(async () => await LoadData());
+
         this.repository = repository;
         Task.Run(() => LoadData());
     }
-    private void LoadData()
+    private async Task LoadData()
     {
-
+        var items = await repository.GetItems();
+        var itemViewModels = items.Select(i =>
+        CreateTodoItemViewModel(i));
+        Items = new ObservableCollection<TodoItemViewModel>
+        (itemViewModels);
     }
+
+    private TodoItemViewModel CreateTodoItemViewModel(TodoItem item)
+    {
+        var itemViewModel = new TodoItemViewModel(item);
+        itemViewModel.ItemStatusChanged += ItemStatusChanged;
+        return itemViewModel;
+    }
+
+    private void ItemStatusChanged(object sender, EventArgs e)
+    {
+    }
+
+    public ICommand AddItem => new Command(async () =>
+    {
+        var itemView = Resolver.Resolve<ItemView>();
+        await Navigation.PushAsync(itemView);
+    });
+
+    public ObservableCollection<TodoItemViewModel> Items { get; private set; }
 }
